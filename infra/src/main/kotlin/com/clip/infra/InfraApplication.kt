9592862f3bae076b60ac734008be304864ea47e2
jang-199 <jw1010110@gmail.com>
2025-09-02 21:55:46 +0900

    init

---
+++ infra/src/main/kotlin/com/clip/infra/InfraApplication.kt
@@ -0,0 +1,11 @@
+package com.clip.infra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfraApplication
+
+fun main(args: Array<String>) {
+    runApplication<InfraApplication>(*args)
+}
