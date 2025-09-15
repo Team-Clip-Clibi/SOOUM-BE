9d0026d95adc7fdcf8cc6ca429b059056d4a4829
jang-199 <jw1010110@gmail.com>
2025-09-15 21:09:31 +0900

    add path /api

--- boot/api/src/main/kotlin/com/clip/api/notification/controller/NotificationController.kt
+++ boot/api/src/main/kotlin/com/clip/api/notification/controller/NotificationController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/api/notifications")
 class NotificationController(
     private val notificationUseCase: NotificationUseCase
 ) {
