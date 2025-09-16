6931d61357e456fcad059e810fb9c38971f53e01
jang-199 <jw1010110@gmail.com>
2025-09-16 20:42:13 +0900

    문서 작성

--- boot/api/src/main/kotlin/com/clip/api/docs/card/FeedDocs.kt
+++ boot/api/src/main/kotlin/com/clip/api/docs/card/FeedDocs.kt
@@ -7,10 +7,10 @@ import org.springframework.http.ResponseEntity
 
 @Tag(name = "Feed", description = "피드 조회 API")
 interface FeedDocs {
-    @Operation(summary = "로그인 API", description = """
-        - 사용자가 로그인할 수 있도록 합니다. 가입 가능 여부 확인 API에서 가입이 되어있는 사용자로 확인된 경우에만 로그인이 가능합니다.
-        - 가입이 되어있는 사용자 : 가입 가능 여부 확인 API에서 banned, withdrawn, registered 값이 모두 false인 사용자
-        - accessToken과 refreshToken을 발급합니다.
+    @Operation(summary = "인기 카드 피드 API", description = """
+        - 인기 카드 피드 조회 API.
+        - 해당 피드는 최대 200개의 카드 정보를 한번에 반환합니다. (최대 개수는 변경될 수 있습니다.)
+        - 페이징을 지원하지 않습니다.
     """)
     fun getPopularFeed(latitude: Double?, longitude: Double?, userId: Long): ResponseEntity<List<FeedResponse>>
 }
