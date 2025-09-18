8eb0ff08fdf7c0e6020419eb52195fc7ab11f654
isprogrammingfun <wisjang1@naver.com>
2025-09-18 16:56:58 +0900

    refactor: 디스크립션 수정

--- boot/api/src/main/kotlin/com/clip/api/docs/img/ImageDocs.kt
+++ boot/api/src/main/kotlin/com/clip/api/docs/img/ImageDocs.kt
@@ -12,7 +12,7 @@ interface ImageDocs {
     @Operation(summary = "프로필 이미지 업로드 API", description = """
         - 사용자가 프로필 이미지를 업로드할 수 있도록 합니다.
         - 이미지는 S3에 저장되며, 저장된 이미지의 URL을 반환합니다.
-        - 이미지 파일은 JPEG, PNG 형식만 허용됩니다.
+        - 이미지 파일은 JPEG 형식만 허용됩니다.
         - Rekognition을 사용하여 부적절한 이미지인지 검사를 수행합니다.
     """)
     fun uploadProfileImage(imageUploadRequest: ImageUploadRequest): ResponseEntity<ImageUrlInfoResponse>
