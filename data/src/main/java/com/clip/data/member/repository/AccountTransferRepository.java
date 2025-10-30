d50c1200c71b503e623cad12d50d51325c4bfacc
isprogrammingfun <wisjang1@naver.com>
2025-10-31 00:34:33 +0900

    refactor: transerId 존재 여부 jpa 메서드로 수정

--- data/src/main/java/com/clip/data/member/repository/AccountTransferRepository.java
+++ data/src/main/java/com/clip/data/member/repository/AccountTransferRepository.java
@@ -7,15 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface AccountTransferRepository extends JpaRepository<AccountTransfer, Long> {
+    boolean existsByTransferIdAndExpirationDateAfter(String transferId, LocalDateTime expirationDate);
+
     Optional<AccountTransfer> findByMember_Pk(Long memberPk);
     @Query("select at from AccountTransfer at join fetch at.member where at.transferId = :transferId and at.expirationDate > current_timestamp")
     Optional<AccountTransfer> findAvailableAccountTransfer(@Param("transferId") String transferId);
-
-    @Query("select case when count(at) > 0 then true else false end from AccountTransfer at where at.transferId = :transferId and at.expirationDate > current_timestamp")
-    boolean validExistsByTransferId(String transferId);
 
     @Modifying
     @Transactional
