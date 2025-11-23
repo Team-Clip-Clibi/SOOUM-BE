d6c0308257d0036a54bc47e32704068f580bf9a2
isprogrammingfun <wisjang1@naver.com>
2025-11-24 00:33:05 +0900

    fix : 계정 이관 버그 수정

--- data/src/main/java/com/clip/data/member/repository/AccountTransferHistoryRepository.java
+++ data/src/main/java/com/clip/data/member/repository/AccountTransferHistoryRepository.java
@@ -2,10 +2,16 @@ package com.clip.data.member.repository;
 
 import com.clip.data.member.entity.AccountTransferHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface AccountTransferHistoryRepository extends JpaRepository<AccountTransferHistory, Long> {
 
     Optional<AccountTransferHistory> findByMemberPk(Long memberPk);
+
+    @Modifying
+    @Query("delete from AccountTransferHistory ath where ath.member.pk = :memberPk")
+    void deleteAccountTransferHistoryByMemberPk(Long memberPk);
 }
